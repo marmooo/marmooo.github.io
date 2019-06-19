591be0bcabf7480900a22deba90cf2c247e727b6
marmooo <marmooo@users.noreply.github.com>
2019-06-20 00:00:19 +0900

    fix bug

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 minify --html-keep-document-tags -r src -o docs
 mkdir -p docs/fonts
-ln -sf fonts/aahub_light.woff2 docs/fonts/aahub_light.woff2
-ln -sf fonts/aahub_light.woff  docs/fonts/aahub_light.woff
-ln -sf fonts/aahub_light.ttf   docs/fonts/aahub_light.ttf
+cp static/fonts/aahub_light.ttf   docs/fonts/aahub_light.ttf
+cp static/fonts/aahub_light.woff  docs/fonts/aahub_light.woff
+cp static/fonts/aahub_light.woff2 docs/fonts/aahub_light.woff2
