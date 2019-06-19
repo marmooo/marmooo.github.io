3ccbf620c55eff333044490136ea9a572ca3d4bc
marmooo <marmooo@users.noreply.github.com>
2019-06-20 00:17:19 +0900

    fix bug

--- build.sh
+++
@@ -1,5 +0,0 @@
-minify --html-keep-document-tags -r src -o docs
-mkdir -p docs/fonts
-cp static/fonts/aahub_light.ttf   docs/fonts/aahub_light.ttf
-cp static/fonts/aahub_light.woff  docs/fonts/aahub_light.woff
-cp static/fonts/aahub_light.woff2 docs/fonts/aahub_light.woff2
