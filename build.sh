0c8ebc17bb19f5a1cb5df949bc0a43cf85b64879
marmooo <marmooo@users.noreply.github.com>
2021-06-04 09:33:41 +0900

    update libraries

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 minify -r src -o docs
-cp -r static/fonts docs
-cp -r static/kanjivg docs
-cp -r static/animCJK docs
+# cp -r static/fonts docs
+# cp -r static/kanjivg docs
+# cp -r static/animCJK docs
