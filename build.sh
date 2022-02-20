83db346b37c2229e0c18ac2a86c15fe3c5d7f110
marmooo <marmooo@users.noreply.github.com>
2022-02-20 09:15:08 +0900

    drop-inline-css

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-minify -r src -o docs
+drop-inline-css -r src -o docs
+minify docs/index.html -o docs/index.html
+minify docs/terms/index.html -o docs/terms/index.html
 cp -r src/.well-known docs
 # cp -r static/fonts docs
 # cp -r static/kanjivg docs
