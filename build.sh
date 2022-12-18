dcdd2f7383fbce299a9081ad47682591d4d86fa5
marmooo <marmooo@users.noreply.github.com>
2022-12-18 20:15:19 +0900

    improve

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
+mkdir -p docs
 cp -r src/* docs
 drop-inline-css -r src -o docs
 minify docs/index.html -o docs/index.html
 minify docs/terms/index.html -o docs/terms/index.html
 cp -r src/.well-known docs
-# cp -r static/fonts docs
-# cp -r static/kanjivg docs
-# cp -r static/animCJK docs
+# cp -r vendor/fonts docs
+# rsync -a --delete vendor/kanjivg/kanji/ docs/kanjivg/
+# rsync -a --delete vendor/animCJK/svgsJa/ docs/animCJK/svgsJa/
+# rsync -a --delete vendor/animCJK/svgsJaSpecial/ docs/animCJK/svgsJaSpecial/
+# rsync -a --delete vendor/animCJK/svgsKana/ docs/animCJK/svgsKana/
