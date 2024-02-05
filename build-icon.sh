ab95dd9043b712409f229e306d7e025ff35dfb32
marmooo <marmooo@users.noreply.github.com>
2024-02-05 10:21:07 +0900

    use Streamline Core

--- build-icon.sh
+++ build-icon.sh
@@ -14,10 +14,10 @@ rsync -a --delete vendor/RemixIcon/icons/ docs/svg/RemixIcon/
 rsync -a --delete vendor/phosphor-icons/assets/regular/ docs/svg/phosphor-icons/regular/
 rsync -a --delete vendor/phosphor-icons/assets/fill/ docs/svg/phosphor-icons/fill/
 rsync -a --delete vendor/tabler-icons/icons/ docs/svg/tabler-icons/
-rsync -a --delete vendor/Solar-icon-set/icons/SVG/Linear/ docs/svg/Solar-icon-set/
 rsync -a --delete vendor/iconoir/icons/ docs/svg/iconoir/
 rsync -a --delete vendor/majesticons/line/ docs/svg/majesticons/line/
 rsync -a --delete vendor/majesticons/solid/ docs/svg/majesticons/solid/
 rsync -a --delete-excluded --exclude="*.json" vendor/lucide/icons/ docs/svg/lucide/
+rsync -a --delete vendor/streamline-vectors/core/line/ docs/svg/streamline-vectors/core/line/
 # kanjis
 deno --allow-read --allow-write build-kanji.js
